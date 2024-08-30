d2820392313d2b20774cbbf6a642e48db228ab83
chonny <hitsugiya@gmail.com>
2024-08-30 13:44:58 -0400

    add Player module

--- src/player.js
+++ src/player.js
@@ -1,6 +1,31 @@
 import { Gameboard } from './gameboard'
 
-export const Player = (gridElement) => {
-  const playerBoard = Gameboard()
-  return { playerBoard, gridElement }
+export const Player = (name, isComputer = false) => {
+  const playerName = name
+  const computer = isComputer
+  const board = Gameboard()
+
+  const getBoard = () => board
+  const getPlayerName = () => playerName
+  const isComputerPlayer = () => computer
+
+  const attack = (coord, enemyBoard) => {
+    if (!computer) {
+      return enemyBoard.recieveAttack(coord)
+    } else {
+      let randomCoord
+      do {
+        randomCoord = [
+          Math.floor(Math.random() * 10),
+          Math.floor(Math.random() * 10),
+        ]
+      } while (
+        enemyBoard.getShotsfired().some((space) => {
+          return space[0] === randomCoord[0] && space[1] === randomCoord[1]
+        })
+      )
+      return enemyBoard.recieveAttack(randomCoord)
+    }
+  }
+  return { getPlayerName, getBoard, isComputerPlayer, attack }
 }
